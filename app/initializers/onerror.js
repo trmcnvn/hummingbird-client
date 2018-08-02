1cb76ce88ddd281da82f030632426678cd0d5492
Thomas McNiven <hello@vevix.net>
2018-08-02 21:14:08 +1000

    Clean the bucket if it becomes corrupted.

--- app/initializers/onerror.js
+++ app/initializers/onerror.js
@@ -1,11 +1,22 @@
 import Ember from 'ember';
 import { later } from '@ember/runloop';
+import { SchemaError } from '@orbit/data';
 
-export function initialize() {
+export function initialize(application) {
   Ember.onerror = function(error) {
-    // Safari issue with pushState
+    // Safari issue with pushState, this can happen if the user spends a lot of time
+    // on the advanced filtering page which can manipulate queryParams a lot.
     if (error.name === 'SecurityError' && error.code === 18) {
       return later(() => window.location.reload(), 1);
+    }
+
+    // Orbit bucket was corrupted?
+    if (error instanceof SchemaError) {
+      console.debug('Bucket became corrupted. Cleaning it up...', error); // @Debug
+      const bucket = application.__container__.lookup('data-bucket:main');
+      return bucket.setItem('store-requests', []).then(() => {
+        return later(() => window.location.reload(), 1);
+      });
     }
 
     // Log to browser console
